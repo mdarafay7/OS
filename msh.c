a50ab04288fd8b9a4590b85deb7e6d201c393c74
mdarafay7 <mdarafay@gmail.com>
2019-01-30 15:42:45 -0600

    Bash_history file output working

--- msh.c
+++ msh.c
@@ -7,86 +7,74 @@
 #include <signal.h>
 
 #define CMD_LOG "bash_history"
-#define WHITESPACE " \t\n"      // We want to split our command line up into tokens                                // will separate the tokens on our command line
-#define MAX_COMMAND_SIZE 255    // The maximum command-line size
-#define MAX_NUM_ARGUMENTS 5     // Mav shell only supports five arguments
+#define WHITESPACE " \t\n" // We want to split our command line up into tokens                                // will separate the tokens on our command line
+#define MAX_COMMAND_SIZE 255 // The maximum command-line size
+# define MAX_NUM_ARGUMENTS 5 // Mav shell only supports five arguments
 
-int main(void)
-{
+int main(void) {
+  FILE * fp;
 
-  char *token[MAX_NUM_ARGUMENTS];
+  char * token[MAX_NUM_ARGUMENTS];
   //while(*token!="exit"){
   char input[100];
-  char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
-//while(cmd_str!="exit"){
+  char * cmd_str = (char * ) malloc(MAX_COMMAND_SIZE);
+  fp = fopen("bash_history", "w+");
+  while (cmd_str != "exit") {
 
-  printf("\nmsh>");
+    printf("\nmsh>");
 
+    while (!fgets(cmd_str, MAX_COMMAND_SIZE, stdin));
 
-      while( !fgets (cmd_str, MAX_COMMAND_SIZE, stdin) );
-
-
-
-
-      int   token_count = 0;
-      char *arg_ptr;
-      char *working_str  = strdup( cmd_str );
-      char *working_root = working_str;
-      while ( ( (arg_ptr = strsep(&working_str, WHITESPACE ) ) != NULL) &&
-                (token_count<MAX_NUM_ARGUMENTS))
-      {
-        token[token_count] = strndup( arg_ptr, MAX_COMMAND_SIZE );
-        if( strlen( token[token_count] ) == 0 )
-        {
-          token[token_count] = NULL;
-        }
-          token_count++;
+    int token_count = 0;
+    char * arg_ptr;
+    char * working_str = strdup(cmd_str);
+    char * working_root = working_str;
+    while (((arg_ptr = strsep( & working_str, WHITESPACE)) != NULL) &&
+      (token_count < MAX_NUM_ARGUMENTS)) {
+      token[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE);
+      if (strlen(token[token_count]) == 0) {
+        token[token_count] = NULL;
       }
-      token[token_count-1]=NULL;
+      token_count++;
+    }
+    token[token_count - 1] = NULL;
 
-      int token_index  = 0;
-      for( token_index = 0; token_index < token_count; token_index ++ )
-      {
-        //printf("token[%d] = %s\n", token_index, token[token_index] );
+    int token_index = 0;
+    for (token_index = 0; token_index < token_count; token_index++) {
+      //printf("token[%d] = %s\n", token_index, token[token_index] );
+    }
+    free(working_root);
+
+    for (int i = 0; i <= token_count; i++) {
+      fprintf(fp, token[i]);
+      fprintf(fp, " ");
+    }
+    fclose(fp);
+    char file[1];
+    file[0] = * token[0];
+    pid_t pid = fork();
+
+    if (pid == 0) {
+
+      //  int res = execvp(token[0], token);
+
+      if (execvp(token[0], token) == -1) {
+
       }
-  free( working_root );
 
+      if (execvp("/usr/local/bin ", token) == -1) {
 
-char file[1];
-file[0]=*token[0];
-  pid_t pid=fork();
+      }
 
-  if (pid==0)
-  {
+      if (execvp("/usr/bin", token) == -1) {
 
-//  int res = execvp(token[0], token);
-  if (token[0]=="history") {
+      }
+      if (execvp("/bin", token) == -1) {
 
-    execvp("/usr/bin",)
+      }
+
+      printf("\nCommand Not Found");
+    }
   }
-  if (execvp(token[0],token) == -1)
-  {
-
-
-  }
-
-  if(execvp("/usr/local/bin ",token)== -1)
-  {
-
-  }
-
-  if(execvp("/usr/bin",token) == -1)
-  {
-
-  }
-  if(execvp("/bin",token) == -1)
-  {
-
-  }
-
-printf("\nCommand Not Found");
-   }
-//}
-exit(0);
 
 }
